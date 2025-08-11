5760eed0d0a636fdc0764871505f62233981a811
MD <md@example.com>
2025-08-11 16:09:55 +0600

    loing done

--- js/login.js
+++ js/login.js
@@ -1,4 +1,16 @@
 document.getElementById('Submit-button').addEventListener('click',function(){
-    console.log('button clicked');
+    const userEmailField = document.getElementById('user-email')
+   const email = userEmailField.value;
+   console.log(email);
+
+  const userpasswordField = document.getElementById('user-password');
+   const password = userpasswordField.value;
+   console.log(password);
+   if(email =="admin@gmail.com" && password == 123456){
+    window.location.href = "./banking.html";
+   }
+   
+
+   
     
 })
